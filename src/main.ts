691d16d35707a3217b576cdc375227e773a1ec62
Equilibrium <miltos-05032006@hotmail.com>
2025-11-06 12:51:59 +0100

    random bs

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,28 @@
 import './style.css'
 
+
+
+
+
+class planet {
+  static forEach(arg0: (planet: string) => void) {
+    throw new Error('Method not implemented.');
+  }
+  planet:string;
+
+   constructor(planet:string) {
+    this.planet = planet
+   }
+
+
+}
+
+planet.forEach((planet:string) => {
+  const planetContainer = document.createElement("div");
+  const name = document.createElement("h1");
+
+
+  name.innerHTML = name.planets;
+
+});
+  
